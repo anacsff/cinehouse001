311de727a55cfd3dabd7bb2158d9ae9fb459eeba
anacsff <anacsfonseca@outlook.com.br>
2022-04-19 21:58:33 -0300

    Criado função para listar todos os filmes

--- cinema.js
+++ cinema.js
@@ -1,2 +1,10 @@
 var catalogo = require("./database/catalogo.json");
-console.log(catalogo.data);
+//console.log(catalogo.data);
+
+function listarTodosOsFilmes (filmes){
+    for(let i = 0; i < filmes.length; i++){
+        console.log(filmes[i].titulo)
+    }
+}
+
+listarTodosOsFilmes(catalogo.data);
