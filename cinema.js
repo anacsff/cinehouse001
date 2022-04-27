e64402413e4ceb07d79ee3bdf4cfde99b6de63c2
anacsff <anacsfonseca@outlook.com.br>
2022-04-26 21:30:03 -0300

    Adicionado Função: Alterar Status em Cartaz

--- cinema.js
+++ cinema.js
@@ -14,3 +14,17 @@ function listarFilmesEmCartaz(filmes){
 }
 
 console.log(listarFilmesEmCartaz(catalogo.data));
+
+function alterarStatusEmCartaz(id, filmes){
+    const idxFilme = filmes.findIndex(filme => filme.codigo === id)
+    //percorre a array e retorna o Index caso o código do filme seja igual ao id dado como parametro
+    if(idxFilme >= 0){
+        filmes[idxFilme].emCartaz = !filmes[idxFilme].emCartaz;
+        return true;
+    }
+    //SE o Index encontrado for maior ou igual a zero (pois quer dizer que o id existe em alguma posição), irá acessar a chave emCartaz do objeto em questão e irá atribuir o valor contrario do valor inicial, se for true virará false e vice-versa.
+    else{
+        return false;
+    }
+}
+ console.log(alterarStatusEmCartaz(505, catalogo.data))
